d846ffbb55aeba2271a7a425a191563ddf90a036
tomas arias <tomas.arias2001@gmail.com>
2024-10-04 08:57:12 -0300

    Refactor LoginPage component and handle login errors

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const LoginPage: React.FC = () => {
   const [isDark, setIsDark] = useState<boolean>(false);
+  const [error, setError] = useState('');
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const navigate = useNavigate();
@@ -17,12 +18,17 @@ const LoginPage: React.FC = () => {
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      console.log(email, password);
-      const response = await axios.post('http://localhost:3001/api/login', { email, password });
+      const contrasena = password;
+      const response = await axios.post('http://localhost:3001/api/login', { email, contrasena });
       console.log(response.data);
       navigate('/dashboard');
-    } catch (error) {
-      console.error('Login failed:', error);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error en la respuesta del servidor:', error.response?.data);
+        setError(error.response?.data.message);
+      } else {
+        console.error('Error inesperado:', error);
+      }
     }
   };
 
@@ -66,6 +72,12 @@ const LoginPage: React.FC = () => {
                 Log in
               </Button>
             </form>
+            {error && <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+              <strong className="font-bold text-sm">{error}</strong>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg onClick={() =>setError('')} className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+              </span>
+            </div>}
             <p className="text-center text-small">
               Don't have an account?{' '}
               <Button
