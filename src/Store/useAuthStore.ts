d381b01e256a0ad5f1a91a86c21dd8f4098dbcdb
tomas arias <tomas.arias2001@gmail.com>
2025-01-15 16:02:34 -0300

    Add persistence to auth store

--- src/Store/useAuthStore.ts
+++ src/Store/useAuthStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 // Definición del tipo del estado
 interface AuthState {
@@ -8,11 +9,16 @@ interface AuthState {
     clearAuthData: () => void;
 }
 
-// Creación del store con Zustand
-export const useAuthStore = create<AuthState>((set) => ({
-    id_persona: null,
-    id_restaurante: null,
-    setAuthData: (id_persona, id_restaurante = undefined) =>
-        set({ id_persona, id_restaurante }),
-    clearAuthData: () => set({ id_persona: null, id_restaurante: null }),
-}));
+// Creación del store con persistencia
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
+            id_persona: null,
+            id_restaurante: null,
+            setAuthData: (id_persona, id_restaurante = undefined) =>
+                set({ id_persona, id_restaurante }),
+            clearAuthData: () => set({ id_persona: null, id_restaurante: null }),
+        }),
+        { name: 'auth-storage' }
+    )
+);
