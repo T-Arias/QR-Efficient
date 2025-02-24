139bd94ecd987e65c0aababbba501099ee2e16c2
tomas arias <tomas.arias2001@gmail.com>
2025-02-24 18:04:38 -0300

    add DashboardPage component with header

---
+++ src/Pages/DashboardPage/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../Components";
+
+const DashBoardPage: React.FC = () => {
+
+  return (
+    <Header title="QR-Efficient Dashboard" />
+  );
+};
+
+export default DashBoardPage;
