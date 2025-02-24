9c6409622cb9138aec157067877aa8f630f8333e
tomas arias <tomas.arias2001@gmail.com>
2025-02-24 03:12:29 -0300

    Update AuthState to include grupo and modify setAuthData method

--- src/Store/useAuthStore.ts
+++ src/Store/useAuthStore.ts
@@ -5,7 +5,8 @@ import { persist } from 'zustand/middleware';
 interface AuthState {
     id_persona: number | null;
     id_restaurante: number | null;
-    setAuthData: (id_persona: number, id_restaurante?: number) => void;
+    grupo: string | null;
+    setAuthData: (id_persona: number, grupo?: string, id_restaurante?: number) => void;
     clearAuthData: () => void;
 }
 
@@ -15,9 +16,10 @@ export const useAuthStore = create<AuthState>()(
         (set) => ({
             id_persona: null,
             id_restaurante: null,
-            setAuthData: (id_persona, id_restaurante = undefined) =>
-                set({ id_persona, id_restaurante }),
-            clearAuthData: () => set({ id_persona: null, id_restaurante: null }),
+            grupo: null,
+            setAuthData: (id_persona, grupo, id_restaurante = undefined) =>
+                set({ id_persona, grupo, id_restaurante }),
+            clearAuthData: () => set({ id_persona: null, id_restaurante: null, grupo:null }),
         }),
         { name: 'auth-storage' }
     )
